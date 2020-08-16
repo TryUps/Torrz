c8c1cf50ae7d556628435d9f266f1d4310fdc3de
Arilson B. Ferreira <arilsonbrj@gmail.com>
2020-08-16 15:34:40 -0300

    Added nedb localstorage database

--- src/components/TorrentList/index.tsx
+++ src/components/TorrentList/index.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
-const TorrentList = styled.div`
+const TorrentList = styled.section`
+  width: 100vw;
+  height: 100vh;
   margin: 0 auto;
   padding: 20px;
-  background: blue;
+  background-color: blue;
 `;
 
 export default TorrentList;
